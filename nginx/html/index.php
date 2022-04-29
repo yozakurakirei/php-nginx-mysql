f406861440773653dc47ef17672423f4f0cdfaf8
yozakura <matchnokaori@gmail.com>
2022-04-29 10:33:41 +0900

    READMEファイル修正

---
+++ nginx/html/index.php
@@ -0,0 +1,18 @@
+<?php
+const HOSTNAME = '?';
+const DATABASE = '?';
+const USERNAME = '?';
+const PASSWORD = '?';
+
+try {
+
+  $pdo = new PDO('mysql:host=' . HOSTNAME . ';dbname=' . DATABASE, USERNAME, PASSWORD);
+  $msg = 'MySQLに接続成功！';
+
+} catch (PDOException $e) {
+
+  $msg  = 'MySQLへの接続失敗...<br>' . $e->getMessage();
+
+}
+
+echo $msg;
